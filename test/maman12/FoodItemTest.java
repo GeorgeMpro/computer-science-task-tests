301343bff56027732bb171412a1005af1d8282ff
George <George.mpro87@gmail.com>
2019-12-10 11:31:00 +0200

    Fixed

--- test/maman12/FoodItemTest.java
+++ test/maman12/FoodItemTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Adjusted to task requirements and limitations.
